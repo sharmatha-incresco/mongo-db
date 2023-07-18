e22d856f603fad1c637eb5d8b067383ec3709eec
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-07-18 13:26:53 +0530

    f

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3011);
+  await app.listen(3000);
 }
 
 bootstrap();
