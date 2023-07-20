3f67339cfa14eb5e2a0ed83343094d62bd29eb72
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-07-20 15:49:46 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,PUT,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
