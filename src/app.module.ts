558f6f84d8c26146c5d9479999df0ea1be43c9ea
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-07-20 16:33:26 +0530

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { JobGetController } from './job/jobget.controller';
 import { JobPostController } from './job/jobpost.controller';
 import { JobGetService } from './job/jobget.service';
 import { JobPostService } from './job/jobpost.service';
-import { AuthModule } from './auth/auth.module';// Replace 'path/to/auth.module' with the actual path to your auth.module.ts file
+
 
 @Module({
   imports: [
@@ -14,7 +14,6 @@ import { AuthModule } from './auth/auth.module';// Replace 'path/to/auth.module'
     MongooseModule.forRoot('mongodb+srv://sharmatha:123@cluster0.fbajy5t.mongodb.net/jobpost'),
     MongooseModule.forFeature([{ name: 'JobGet', schema: JobGetSchema }]),
     MongooseModule.forFeature([{ name: 'JobPost', schema: JobPostSchema }]),
-    AuthModule, // Add the AuthModule to the imports array
   ],
   controllers: [JobGetController, JobPostController],
   providers: [JobGetService, JobPostService],
