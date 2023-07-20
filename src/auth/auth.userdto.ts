95a49ec0dac275aa020190330bd2695b48b8eed6
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-07-20 16:39:51 +0530

    fix

--- src/auth/auth.userdto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class User {
-    @ApiProperty()
-    email: string;
-    @ApiProperty()
-    password: string;
-}
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,PUT,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
